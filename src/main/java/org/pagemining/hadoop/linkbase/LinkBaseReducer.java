98dd59fdd23b663e2bb4516b716b493d3427abd7
Liang Xiang <xlvector@gmail.com>
2014-01-26 15:08:04 +0800

    add is download

--- src/main/java/org/pagemining/hadoop/linkbase/LinkBaseReducer.java
+++ src/main/java/org/pagemining/hadoop/linkbase/LinkBaseReducer.java
@@ -8,21 +8,24 @@ import org.apache.hadoop.mapred.Reducer;
 import org.apache.hadoop.mapred.Reporter;
 
 import java.io.IOException;
+import java.util.HashSet;
 import java.util.Iterator;
+import java.util.Set;
 
 public class LinkBaseReducer extends MapReduceBase implements Reducer<Text, Text, Text, Text> {
 
     @Override
     public void reduce(Text key, Iterator<Text> values, OutputCollector<Text, Text> collector, Reporter reporter) throws IOException {
-        int n = 0;
+        Set<String> sources = new HashSet<String>();
+        boolean hasDownload = false;
         while (values.hasNext()){
-            n += 1;
             LinkInfo linkInfo = new LinkInfo();
             linkInfo.fromString(values.next().toString());
+            sources.add(linkInfo.getSrcLink());
             if(linkInfo.getUpdatedAt() > 0) {
-                return;
+                hasDownload = true;
             }
         }
-        collector.collect(new Text(key), new Text(String.valueOf(n)));
+        collector.collect(new Text(key), new Text(String.valueOf(sources.size()) + "\t" + String.valueOf(hasDownload)));
     }
 }
