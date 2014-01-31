4f6c64d9ef644f14cb76cdb12d8b031a0781f4dc
Liang Xiang <xlvector@gmail.com>
2014-01-31 15:46:40 +0800

    xpath extractor

--- src/main/java/org/pagemining/hadoop/infoextract/XPathExtractorConfig.java
+++ src/main/java/org/pagemining/hadoop/infoextract/XPathExtractorConfig.java
@@ -8,13 +8,8 @@ public class XPathExtractorConfig {
     private List<SiteConfig> sites = new ArrayList<SiteConfig>();
 
     public XPathExtractorConfig(){
-        try {
-            BufferedReader reader = new BufferedReader(
-                    new InputStreamReader(XPathExtractorConfig.class.getResourceAsStream("/extract.config")));
-            SiteConfig site = new SiteConfig();
-            while (true){
-                String line = reader.readLine();
-                if (null == line) break;
+        SiteConfig site = new SiteConfig();
+            for(String line : Config.data){
                 line = line.trim();
                 if (line.length() == 0){
                     if(site.getName() != null && site.getName().length() > 0){
@@ -38,14 +33,6 @@ public class XPathExtractorConfig {
             if(site.getName() != null && site.getName().length() > 0){
                 getSites().add(site);
             }
-            reader.close();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-            System.exit(1);
-        } catch (IOException e) {
-            e.printStackTrace();
-            System.exit(1);
-        }
     }
 
     public List<SiteConfig> getSites() {
