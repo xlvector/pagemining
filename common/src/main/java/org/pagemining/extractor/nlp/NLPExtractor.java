9883679890f13db23e064e82d3ff0de4cb381c60
Liang Xiang <xlvector@gmail.com>
2014-02-19 14:28:58 +0800

    change xpath json

---
+++ common/src/main/java/org/pagemining/extractor/nlp/NLPExtractor.java
@@ -0,0 +1,21 @@
+package org.pagemining.extractor.nlp;
+
+import net.minidev.json.JSONObject;
+import org.ansj.domain.Term;
+import org.ansj.splitWord.analysis.NlpAnalysis;
+import org.jsoup.nodes.Document;
+import org.pagemining.extractor.Extractor;
+
+import java.util.List;
+
+public class NLPExtractor implements Extractor {
+    @Override
+    public JSONObject extract(String url, String html) {
+        return null;
+    }
+
+    @Override
+    public JSONObject extract(String url) {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+}
