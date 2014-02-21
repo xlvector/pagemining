1aa601b2918de2c099ca8e6f0f9ba6ad8d7f426e
Liang Xiang <xlvector@gmail.com>
2014-02-21 17:22:48 +0800

    debug

--- hadoop/src/main/java/org/pagemining/hadoop/dict/DictExtractorMapper.java
+++ hadoop/src/main/java/org/pagemining/hadoop/dict/DictExtractorMapper.java
@@ -25,6 +25,16 @@ public class DictExtractorMapper extends TableMapper<Text, Text> {
         return sb.toString();
     }
 
+    private boolean EqualsUTF8(String a, String b) throws UnsupportedEncodingException {
+        byte[] ba = a.getBytes("UTF-8");
+        byte[] bb = a.getBytes("UTF-8");
+        if(ba.length != bb.length) return false;
+        for(int i = 0; i < ba.length; ++i){
+            if(ba[i] != bb[i]) return false;
+        }
+        return true;
+    }
+
     @Override
     public void map(ImmutableBytesWritable row, Result value, Context context) throws InterruptedException, IOException {
         String url = new String(value.getValue("data".getBytes(), "url".getBytes()), "UTF-8");
@@ -39,10 +49,8 @@ public class DictExtractorMapper extends TableMapper<Text, Text> {
         if(jsonObject == null) return;
 
         for(Map.Entry<String, Object> e : jsonObject.entrySet()){
-            context.write(new Text(encode(e.getKey())), new Text("1"));
-            context.write(new Text(encode("相关词条")), new Text("1"));
-            /*
-            if(!encode(e.getKey()).equals(encode("相关词条"))) continue;
+
+            if(!EqualsUTF8(e.getKey(), "相关词条")) continue;
             Object obj = e.getValue();
             if(obj instanceof String){
                 context.write(new Text((String)obj), new Text("1"));
@@ -54,7 +62,6 @@ public class DictExtractorMapper extends TableMapper<Text, Text> {
                     }
                 }
             }
-            */
         }
     }
 }
