02a084ebddeedeeecd70c5a354f75b7805125d41
Liang Xiang <xlvector@gmail.com>
2014-02-26 13:57:50 +0800

    domain group

--- hadoop/src/main/java/org/pagemining/hadoop/domain/DomainGroupTask.java
+++ hadoop/src/main/java/org/pagemining/hadoop/domain/DomainGroupTask.java
@@ -9,22 +9,17 @@ import org.apache.hadoop.io.compress.CompressionCodec;
 import org.apache.hadoop.io.compress.GzipCodec;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.output.MultipleOutputs;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 
 import java.io.IOException;
 
 public class DomainGroupTask {
-    public static class Map extends Mapper<LongWritable, Text, NullWritable, Text>{
-        private MultipleOutputs mos;
-
-        @Override
-        public void setup(Context context) throws IOException, InterruptedException{
-            super.setup(context);
-            mos = new MultipleOutputs(context);
-        }
+    public static class Map extends Mapper<LongWritable, Text, Text, Text>{
 
         @Override
         protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
@@ -34,7 +29,25 @@ public class DomainGroupTask {
             String url = tks[1];
             String domain = DomainUtil.getDomain(url);
             if(domain != null){
-                mos.write(NullWritable.get(), value, "test");
+                context.write(new Text(domain), value);
+            }
+        }
+    }
+
+    public static class Reduce extends Reducer<Text, Text, NullWritable, Text> {
+        private MultipleOutputs<NullWritable, Text> mos;
+
+        @Override
+        public void setup(Context context) throws IOException, InterruptedException{
+            super.setup(context);
+            mos = new MultipleOutputs<NullWritable, Text>(context);
+        }
+
+        @Override
+        protected void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+            String domain = key.toString();
+            for(Text value : values){
+                mos.write(NullWritable.get(), value, domain);
             }
         }
 
@@ -57,11 +70,15 @@ public class DomainGroupTask {
         FileOutputFormat.setOutputPath(job, new Path(output));
         job.setJarByClass(DomainGroupTask.class);
         job.setMapperClass(Map.class);
+        job.setReducerClass(Reduce.class);
         job.setInputFormatClass(TextInputFormat.class);
+        job.setOutputFormatClass(TextOutputFormat.class);
+        job.setMapOutputKeyClass(Text.class);
+        job.setMapOutputValueClass(Text.class);
         job.setOutputKeyClass(NullWritable.class);
         job.setOutputValueClass(Text.class);
 
-        job.setNumReduceTasks(0);
+        job.setNumReduceTasks(16);
         job.waitForCompletion(true);
     }
 }
